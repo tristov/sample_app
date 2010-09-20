1075ed8a026144b043246a085e013b907d4ff73a
Tode Ristov <toderistov@hotmail.com>
2010-09-20 09:52:41 +0200

    removed the index page

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,14 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.home    '/home',    :controller => 'pages', :action => 'home'
+  # You can have the root of your site routed with map.root
+  # -- just remember to delete public/index.html.
+  map.root :controller => 'pages', :action => 'home'
+
+
 end
