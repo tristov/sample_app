d587b4ad87562696275dc98a2693abfda181df91
Tode Ristov <toderistov@hotmail.com>
2010-09-20 10:41:06 +0200

    finishing the layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -40,14 +40,14 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-
+  # You can have the root of your site routed with map.root
+  # -- just remember to delete public/index.html.
+  map.root :controller => 'pages', :action => 'home'
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.home    '/home',    :controller => 'pages', :action => 'home'
-  # You can have the root of your site routed with map.root
-  # -- just remember to delete public/index.html.
-  map.root :controller => 'pages', :action => 'home'
+  map.signup  '/signup',  :controller => 'users', :action => 'new'
 
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
   integrate_views
 
   before(:each) do
-    @base_title = 'Ruby on Rails Tutorial Sample App'
+    @base_title = 'Sample App'
   end
   
   describe "GET 'home'" do
