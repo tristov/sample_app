4bcd8b0a725acdabeddf361a87abbe917b2ae7b2
tristov <toderistov@hotmail.com>
2010-09-26 23:49:49 +0200

    User signup complete, chapter 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,14 +35,78 @@ describe UsersController do
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
+    end
+    it "should have the right title" do
+      get :new
+      response.should have_tag("title", /Sign up/)
     end
   end
 
-  it "should have the right title" do
-      get 'new'
-      response.should have_tag("title", /Sign up/)
+  describe "post 'create'" do
+    describe "failure"do
+      before(:each)do
+        @attr={
+          :name => "",
+          :email => "",
+          :password => "",
+          :password_confirmation=>""}
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+      it "should have right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /Sign up/i)
+      end
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template(:new)
+      end
     end
+    describe "success" do
 
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
